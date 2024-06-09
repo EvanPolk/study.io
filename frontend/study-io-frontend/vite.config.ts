4ad68bb87cf509b779e1a6d52ed977befc94b385
Evan <evanpolk2@gmail.com>
2024-06-09 17:34:02 -0600

    Fixing vite.config.ts

--- frontend/study-io-frontend/vite.config.ts
+++ frontend/study-io-frontend/vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/study.io/frontend/study-io-frontend"
-})
+  base: '/', // Set the base to root
+  resolve: {
+    alias: {
+      '@components': '/src/components', // Alias for your components directory
+      '@icons': '/src/components/Icons' // Alias for your Icons directory if needed
+    }
+  }
+});
