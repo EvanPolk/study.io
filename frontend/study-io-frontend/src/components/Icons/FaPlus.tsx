634f076be7055665c44f087b5007530b59677499
Evan <evanpolk2@gmail.com>
2024-06-09 17:46:39 -0600

    Versioning icons

---
+++ frontend/study-io-frontend/src/components/Icons/FaPlus.tsx
@@ -0,0 +1,16 @@
+function FaPlus() {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth={0}
+      viewBox="0 0 448 512"
+      height="1em"
+      width="1em"
+    >
+      <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
+    </svg>
+  );
+}
+
+export default FaPlus;
